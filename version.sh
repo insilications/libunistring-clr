54c4a30c90f5e34e8fbbc78f0718e9aadcefbb67
insilications <boboniboni@gmail.com>
2020-08-25 20:40:58 -0300

    Add pkg-config file support
    Small fixes & housekeeping.
    
    Small fixes & housekeeping.
    
    Small fixes & housekeeping.
    
    Small fixes & housekeeping.
    
    Small fixes & housekeeping.

--- version.sh
+++ version.sh
@@ -4,3 +4,5 @@ RELEASE_DATE=2018-05-17      # in "date +%Y-%m-%d" format
 
 # Version of gnulib that was used in this release.
 GNULIB_GIT_COMMIT=b35c22af02271c8dd9844b98db17702b8bf2bd93
+
+echo $VERSION_NUMBER
